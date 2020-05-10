f32c6f42bd090e7abf009223393d55a6dc12a0f9
julianarosa <julianarosa@brq.com>
2020-05-09 22:04:28 -0300

    project started

---
+++ site-design-ativista/src/home-temp/home-temp.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './home-temp.scss';
+
+export class HomeTemp extends Component {
+  render() {
+    return (
+      <main className="home__temp">
+        <h1>Em breve site Design Ativista estará no ar.</h1>
+        <p>Enquanto isso, você pode conferir o que a gente fez e compartilhar as artes produzidas.</p>
+
+        <p>Veja o que fizemos para enfrentar a crise do COVID-19:</p>
+        <a>escolha uma arte aqui</a>
+        <label>Quer disponibilizar sua arte através do Design Ativista? <a>Salve o arquivo aqui</a></label>
+        <footer>
+          <img />
+          <a> </a>
+        </footer>
+      </main>
+    )
+  }
+};
+
+export default HomeTemp;
